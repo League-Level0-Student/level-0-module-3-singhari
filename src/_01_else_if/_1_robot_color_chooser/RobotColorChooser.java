b340c9730c5721c50a6426ce8a014abcf799c4d7
league <league@imac-14.attlocal.net>
2023-08-02 19:59:19 -0700

    started 0.3

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,11 +3,56 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
+import java.util.Random;
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+		Robot robt = new Robot(); 
+		robt.penDown();
+		robt.setPenWidth(10);
+		String re = "How many sides";
+		String go = JOptionPane.showInputDialog(null,re);
+		int va = Integer.parseInt(go);
+		for(int x=0; x<va; x++) {
+		String cool = "What color would you like?";
+		String color = JOptionPane.showInputDialog(null,cool);
+	
+		if(color.equals("green")) {
+			robt.setPenColor(0,250,0);
+		}
+		else if(color.equals("gray")) {
+			robt.setPenColor(200,200,200);
+		}
+		else if(color.equals("red")) {
+			robt.setPenColor(250,80,80);
+		}
+		else if (color.equals("pink")) {
+			robt.setPenColor(250,80,800);
+		}
+		else {
+			Random trtrtr = new Random();
+			int ran = trtrtr.nextInt(255);
+			Random dr = new Random();
+			int r = dr.nextInt(255);
+			Random drx = new Random();
+			int y = drx.nextInt(255);
+			robt.setPenColor(ran,r,y);
+		}
+	
+		robt.setSpeed(100);
+		//int sides = 3;
+			
+		int angle = 360/va;
+		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
+	robt.move(100);
+robt.turn(angle+1);
+				//  INCREASE COUNT. Increase the count by 1
+		//  End the while loop here
+	}
+
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,6 +3,8 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
@@ -12,14 +14,18 @@ public class CrazyCatLady {
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-		
-		
+		String re = "How many cats do you have?";
+		String go = JOptionPane.showInputDialog(null,re);
+		int va = Integer.parseInt(go);
+			if(va >= 3) {
+				String e = "You're a crazy cat lady.";
+				JOptionPane.showMessageDialog(null,e);
+			}
 		
 		
 		
 	}
-
+//https://www.youtube.com/watch?v=oj_yLBltPE8
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
